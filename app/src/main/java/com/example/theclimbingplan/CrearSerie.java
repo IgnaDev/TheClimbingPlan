58b168a15acdae99761349ecf546eeea17a3adfe
Ignacio Lapeña <nacholape93@gmail.com>
2023-05-31 19:35:23 +0200

    crear series activity

---
+++ app/src/main/java/com/example/theclimbingplan/CrearSerie.java
@@ -0,0 +1,14 @@
+package com.example.theclimbingplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearSerie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_serie);
+    }
+}
